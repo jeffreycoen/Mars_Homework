6608ea838abf0c1dbcfb1ee939efb53582d1e46a
jeffreycoen <jeff.coen@gmail.com>
2017-11-09 14:06:09 -0500

    updating code

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template, jsonify, redirect
 from flask_pymongo import PyMongo
-import scrape_craigslist
+import scrape_mars
 
 app = Flask(__name__)
 
 mongo = PyMongo(app)
+
+conn = "mongodb://admin:tester@ds243335.mlab.com:43335/heroku_kj4scs6n"
+client = PyMongo.mongo(conn)
+db = client.heroku_kj4scs6n
+mars = db.newcollection
 
 
 @app.route("/")
