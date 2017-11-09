d69a56e133c506147e9e0a4fc1e0a8d4f1c81563
jeffreycoen <jeff.coen@gmail.com>
2017-11-09 14:35:34 -0500

    more updates

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify, redirect
-from flask_pymongo import PyMongo
+import pymongo
 import scrape_mars
 
 app = Flask(__name__)
@@ -9,25 +9,25 @@ mongo = PyMongo(app)
 conn = "mongodb://admin:tester@ds243335.mlab.com:43335/heroku_kj4scs6n"
 client = PyMongo.mongo(conn)
 db = client.heroku_kj4scs6n
-mars = db.newcollection
+mars = db.new_collection
 
 
 @app.route("/")
 def index():
-    listings = mongo.db.listings.find_one()
-    return render_template("index.html", listings=listings)
+    listings = mongo.db.mars.find_one()
+    return render_template("index.html", mars_listings=mars_listings)
 
 
 @app.route("/scrape")
 def scrape():
     listings = mongo.db.listings
-    listings_data = scrape_craigslist.scrape()
+    listings_data = scrape_mars.scrape()
     listings.update(
         {},
         listings_data,
         upsert=True
     )
-    return redirect("http://localhost:5000/", code=302)
+    return index
 
 
 if __name__ == "__main__":
--- scrape_mars.py
+++ scrape_mars.py
@@ -13,17 +13,82 @@ def init_browser():
 
 def scrape():
     browser = init_browser()
-    listings = {}
+    mars_dict = {}
 
-    url = "https://raleigh.craigslist.org/search/hhh?max_price=1500&availabilityMode=0"
+
+# Mars News
+    url = "https://mars.nasa.gov/news/"
     browser.visit(url)
     time.sleep(1)
 
     html = browser.html
     soup = BeautifulSoup(html, "html.parser")
 
-    listings["headline"] = soup.find("a", class_="result-title").get_text()
-    listings["price"] = soup.find("span", class_="result-price").get_text()
-    listings["hood"] = soup.find("span", class_="result-hood").get_text()
+    news_title = (news_title.text)
+    mars_dict['news_title'] = news_title
 
-    return listings
+    html = browser.html
+    soup = bs(html, 'html.parser')
+    news_p = soup.find_all('div', class_='article_teaser_body')
+    holding2 = []
+
+    #print(len(news_p))
+
+    for x in news_p:
+        x = x.text
+        #print(x[1])
+        holding2.append(x)
+    
+    news_p = (holding2[0])
+
+    #Append the dictionary
+
+    mars_dict['news_p'] = news_p
+
+
+# Get the image
+    url2 = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    browser_image.visit(url2)
+
+    html = browser_image.html
+    soup = bs(html, 'html.parser')
+    featured_image = soup.find_all('a', class_='button fancybox')
+    for y in featured_image:
+        y = y.text
+        #print(x[1])
+        holding3.append(y)
+
+    mars_dict["image"] = holding3
+
+
+    # Get the weather
+    url_weather = 'https://twitter.com/marswxreport?lang=en'
+    browser_weather.visit(url_weather)
+    html = browser_weather.html
+    soup = bs(html, 'html.parser')
+    mars_weather = soup.find('div', class_='js-tweet-text-container')
+
+    mars_weather = (mars_weather.text)
+    
+    #Append the dictionary
+    mars_dict['mars_weather'] =  mars_weather
+
+
+# Get the mars facts
+    #mars facts
+    url_facts = 'http://space-facts.com/mars/'
+    tables = pd.read_html(url_facts)
+    tables_df = pd.DataFrame(tables[0])
+
+   # tables_df
+    html_table = tables_df.to_html()
+    #html_table
+
+    # Append the dictionary
+    mars_dict["facts_table"] = html_table
+
+
+    print(mars_dict)
+    return mars_dict
+
+
